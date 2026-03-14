125fe35d40d44c50b2418754a4e825ec1f82ae7e
Manav Singla <manavsinghla123@gmail.com>
2026-03-14 12:32:09 +0530

    some fix.

--- server.js
+++ server.js
@@ -34,10 +34,8 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`);
-    });
-}
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 export default app;
